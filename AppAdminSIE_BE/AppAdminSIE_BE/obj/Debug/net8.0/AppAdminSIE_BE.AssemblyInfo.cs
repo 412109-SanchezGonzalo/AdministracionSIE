0cc469b9dd3d31f78a7a75d8bc6faa6ebc193c4e
412109-SanchezGonzalo <4121109@tecnicatura.frc.utn.edu.ar>
2025-08-25 13:45:49 -0300

    Move railway to root Railway Deployment

--- AppAdminSIE_BE/AppAdminSIE_BE/obj/Debug/net8.0/AppAdminSIE_BE.AssemblyInfo.cs
+++ AppAdminSIE_BE/AppAdminSIE_BE/obj/Debug/net8.0/AppAdminSIE_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAdminSIE_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93900989f6e912d75ee6418816934f4345be45bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267e33cf77ef85a4d3eb39692e0adf00dd53482a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAdminSIE_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAdminSIE_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
