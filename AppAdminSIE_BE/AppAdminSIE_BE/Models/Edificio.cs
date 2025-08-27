4d7df3ed8ac6f6b91d97bb1334d0f7230a87cb84
412109-SanchezGonzalo <136187975+412109-SanchezGonzalo@users.noreply.github.com>
2025-08-27 17:17:45 -0300

    Add Edificio Model

---
+++ AppAdminSIE_BE/AppAdminSIE_BE/Models/Edificio.cs
@@ -0,0 +1,10 @@
+﻿namespace AppAdminSIE_BE.Models
+{
+    public class Edificio
+    {
+        public int Id_Edificio { get; set; }
+        public string? Nombre { get; set; }
+        public string Calle {  get; set; }
+        public string Numeracion { get; set; }
+    }
+}
