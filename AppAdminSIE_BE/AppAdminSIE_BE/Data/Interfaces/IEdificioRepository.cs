8744a96b585c9f426a0e5ae8f3145fc404bee437
412109-SanchezGonzalo <136187975+412109-SanchezGonzalo@users.noreply.github.com>
2025-08-27 17:04:43 -0300

    Add IEdificioRepository

---
+++ AppAdminSIE_BE/AppAdminSIE_BE/Data/Interfaces/IEdificioRepository.cs
@@ -0,0 +1,9 @@
+﻿using AppAdminSIE_BE.Models;
+
+namespace AppAdminSIE_BE.Data.Interfaces
+{
+    public interface IEdificioRepository
+    {
+        IEnumerable<Edificio> GetAllEdificios();
+    }
+}
