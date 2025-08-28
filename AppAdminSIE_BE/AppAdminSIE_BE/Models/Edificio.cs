adfef1bc1ef97db3b2a6fd01bb647d3b85bd12cb
412109-SanchezGonzalo <136187975+412109-SanchezGonzalo@users.noreply.github.com>
2025-08-27 23:31:11 -0300

    Update atributo Nombre

--- AppAdminSIE_BE/AppAdminSIE_BE/Models/Edificio.cs
+++ AppAdminSIE_BE/AppAdminSIE_BE/Models/Edificio.cs
@@ -3,8 +3,9 @@
     public class Edificio
     {
         public int Id_Edificio { get; set; }
-        public string? Nombre { get; set; }
+        public string Nombre { get; set; }
         public string Calle {  get; set; }
         public string Numeracion { get; set; }
     }
 }
+
