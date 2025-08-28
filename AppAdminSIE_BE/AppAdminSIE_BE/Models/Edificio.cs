a0acc0f47e431e1573325e7535fe109751334dbc
412109-SanchezGonzalo <4121109@tecnicatura.frc.utn.edu.ar>
2025-08-28 00:10:42 -0300

    Solucion de conflicto

--- AppAdminSIE_BE/AppAdminSIE_BE/Models/Edificio.cs
+++ AppAdminSIE_BE/AppAdminSIE_BE/Models/Edificio.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿namespace AppAdminSIE_BE.Models
 {
     public class Edificio
@@ -10,15 +9,3 @@
     }
 }
 
-=======
-﻿namespace AppAdminSIE_BE.Models
-{
-    public class Edificio
-    {
-        public int Id_Edificio { get; set; }
-        public string? Nombre { get; set; }
-        public string Calle {  get; set; }
-        public string Numeracion { get; set; }
-    }
-}
->>>>>>> ba2289b (Agregar modelo Edificio y actualizar .gitignore)
